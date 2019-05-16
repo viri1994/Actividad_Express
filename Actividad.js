35d6e687372732fe7cbc0c8199f72f7084daebe5
viri1994 <vdlre1994@gmail.com>
2019-05-16 11:59:19 -0500

    Metodos

--- Actividad.js
+++ Actividad.js
@@ -7,4 +7,18 @@ app.all('/hello', (req, res) => res.send('Hello World!'))
 app.all('/bye', (req, res) => res.send('Bye Bye!'))
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
+
+app.get('/receta', (req, res)=>{
+	res.status(555).send("tacos")
+})
+
+app.put('/Nombre', (req, res)=>{
+	res.status(450).send("Viri")
+})
+
+app.post('/menu', (req, res)=>{
+	res.status(404).send({foo: "tacos"})
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
