e9b04f894ff32b52d69907280f7f73bb8c74fce2
Viridiana <vdlre1994@gmail.com>
2019-05-18 17:30:26 -0500

    Programa terminado, endpoint de autenticacion

--- Actividad.js
+++ Actividad.js
@@ -2,33 +2,93 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const jwt = require('jsonwebtoken');
 const bodyParser= require ('body-parser');
 const cookieParser= require ('cookie-parser');
-const jwt = require('jsonwebtoken');
+app.use(bodyParser.json());
+app.use(cookieParser());
+const privateKey ='llave';
 
 
 //ejercicio 4
 let pets = [];
 
-app.use(bodyParser.json());
-app.use(cookieParser());
+//---------------------------------------------------------------------
+//all utiliza todos los metodos 
+app.all('/hello', (req, res) => {
+res.send('Hello World!')
+});
+
+app.all('/bye', (req, res) => {
+res.send('Bye Bye!')
+});
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 
-app.post('/pets', (req, res)=> {
-	console.log(req.body)
+app.get('/receta', (req, res)=>{
+	res.status(555).send("tacos")
+});
+
+app.put('/Nombre', (req, res)=>{
+	res.status(450).send("Viri")
+});
+
+app.post('/menu', (req, res)=>{
+	res.status(404).send({foo: "tacos"})
+});
+
+//Middleware
+
+app.all('*', (req, res, next)=> {
+	console.log('Viri esta haciendo la actividad',req.path)
+	next()
+});
+
+app.get('/dog', (req, res)=>{
+	res.send("ven");
+});
+
+app.get('/dog', (req, res)=>{
+	res.send("ven2");
+});
+
+app.get('/cat', (req, res)=>{
+	res.send("Hola");
+});
+
+app.get('/body',(req, res)=>{
+	console.log(req.body);
+	res.send(req.body);
+});
+
+app.get('/cookie',(req, res)=>{
+	console.log(req.cookie);
+	res.send(req.cookie);
+});
+
+
+app.get('/pets', (req, res)=> {
 	console.log(req.cookies);// esto es para ver las cookies en consola
+	res.status(200).send(pets);
 
-
+});
+	app.post('/pets',(req, res)=>{
+		console.log(req.body);
 	if(req.body.pet && req.body.name) {
 		pets.push(req.body);
-		res.status(201).send('ya quedo');
+		res.status(201).send('ya se agrego');
 	}else{
 		res.status(400).send({error:'Pon correctos los datos'})
 	}
 });
+
+
 //-------------------------------------------------------------------
 app.post('/auth/signin', (req, res)=>{
-	console.log('req.body', req.body);
+	if(!(req.body.user && req.body.pass)){
+		res.status(400).send("Necesitas introducir usuario y contraseña")
+	}
 
 //Generar token y leer token
 
@@ -37,59 +97,28 @@ jwt.sign({ user: req.body.user, theme: 'black' }, privateKey, function(err, toke
       res.send(500).end();
     } else {
       res.status(200).send({token: token})
-    }
+    	}
 	});
   });
 // para verificar si el usuario existe en la base de datos
-
-jwt.verify(req.headers.authorization, privateKey, function(err, decoded) {
+app.use((req,res, next)=>{
+	jwt.verify(req.headers.authorization, privateKey, function(err, decoded) {
     if(err) {
-      res.status(500).end('aqui')
+      res.status(500).end('aqui');
     } else {
-      console.log(decoded)
+      console.log(decoded);
       // checar ese usuario en la base datos a ver si existe
-    }
+    	next()
+		}
+
+	});
+
   });
-
-//---------------------------------------------------------------------
-//all utiliza todos los metodos 
-app.all('/hello', (req, res) => res.send('Hello World!'))
-app.all('/bye', (req, res) => res.send('Bye Bye!'))
-app.use('/assets', express.static(path.join(__dirname, 'assets')))
-
-
-app.get('/receta', (req, res)=>{
-	res.status(555).send("tacos")
-})
-
-app.put('/Nombre', (req, res)=>{
-	res.status(450).send("Viri")
-})
-
-app.post('/menu', (req, res)=>{
-	res.status(404).send({foo: "tacos"})
-})
-
-//Middleware
-
-app.all('*', (req, res, next)=> {
-	console.log('Viri esta haciendo la actividad',req.path)
-	next()
-})
-app.get('/dog', (req, res)=>{
-	res.send("ven");
-})
-
-app.get('/dog', (req, res)=>{
-	res.send("ven2");
-})
-
-app.get('/cat', (req, res)=>{
-	res.send("Hola");
+app.get('ultimo',(req, res)=>{
+	res.send("ultimo")
 })
 
 
 
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Servidor corriendo'))
